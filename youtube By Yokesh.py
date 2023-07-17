64e3cc60093b12556432f5ae422b0b1b61262a05
Yokesh-B <134533121+Yokesh-B@users.noreply.github.com>
2023-07-17 12:44:35 +0530

    Update youtube By Yokesh.py

--- youtube By Yokesh.py
+++ youtube By Yokesh.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import plotly.express as px
-import streamlit as st
-from streamlit_option_menu import option_menu
 import mysql.connector as sql
 import pymongo
 from googleapiclient.discovery import build
 from PIL import Image
-#SETTING THE STREAMLIT PAGE CONFIGURATIONS
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# connection with MongoDB Compass and Creating a  database as (youtube_data)
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["youtube_data"]
+# CONNECT WITH MYSQL WorKbench DATABASE
+mydb = sql.connect(host="localhost",
+                   user="root",
+                   password="Yokesh@1",
+                   database= "youtube",
+                   auth_plugin="mysql_native_password"
+                  )
+mycursor = mydb.cursor(buffered=True)
+#STREAMLIT PAGE 
 icon = Image.open(r'C:\Users\Dell\Documents\data science\youtube_logo.png')
 st.set_page_config(page_title= "Youtube",
                    page_icon= icon,
                    layout= "wide",
                    initial_sidebar_state= "expanded",
                    menu_items={'About': """# app is created by *Yokesh*"""})
-# CREATING SIDEBAR 
+# SIDEBAR 
 with st.sidebar:
     selected = option_menu(None, ["Home","Extract","View"], 
                            icons=["house-door-fill","tools","card-text"],
@@ -24,21 +36,10 @@ with st.sidebar:
                                    "icon": {"font-size": "30px"},
                                    "container" : {"max-width": "6000px"},
                                    "nav-link-selected": {"background-color": "#011cc8"}})
-# Bridging a connection with MongoDB Atlas and Creating a new database(youtube_data)
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["youtube_data"]
-# CONNECTING WITH MYSQL DATABASE
-mydb = sql.connect(host="localhost",
-                   user="root",
-                   password="Yokesh@1",
-                   database= "youtube",
-                   auth_plugin="mysql_native_password"
-                  )
-mycursor = mydb.cursor(buffered=True)
-# BUILDING CONNECTION WITH YOUTUBE API
+# BUILDING WITH YOUTUBE API
 api_key = "AIzaSyDKI8AqPrp4poQwYnqfazLgEqVH5OGHPwQ"
 youtube = build('youtube','v3',developerKey=api_key)
-# FUNCTION TO GET CHANNEL DETAILS
+#  GET CHANNEL DETAILS
 def get_channel_details(channel_id):
     ch_data = []
     response = youtube.channels().list(part = 'snippet,contentDetails,statistics',
@@ -55,7 +56,7 @@ def get_channel_details(channel_id):
                     )
         ch_data.append(data)
     return ch_data
-# FUNCTION TO GET VIDEO IDS
+#  GET VIDEO IDS
 def get_channel_videos(channel_id):
     video_ids = []
     # get Uploads playlist id
@@ -77,7 +78,7 @@ def get_channel_videos(channel_id):
         if next_page_token is None:
             break
     return video_ids
-# FUNCTION TO GET VIDEO DETAILS
+# GET VIDEO DETAILS
 def get_video_details(v_ids):
     video_stats = []
     
@@ -104,7 +105,7 @@ def get_video_details(v_ids):
                                )
             video_stats.append(video_details)
     return video_stats
-# FUNCTION TO GET COMMENT DETAILS
+#  GET COMMENT DETAILS
 def get_comments_details(v_id):
     comment_data = []
     try:
@@ -130,18 +131,16 @@ def get_comments_details(v_id):
     except:
         pass
     return comment_data
-# FUNCTION TO GET CHANNEL NAMES FROM MONGODB
+# GET CHANNEL NAMES FROM MongoDB
 def channel_names():   
     ch_name = []
     for i in db.channel_details.find():
         ch_name.append(i['Channel_name'])
     return ch_name
-# HOME PAGE
+# HOME PAGE of streamlit 
 if selected == "Home":
-    # Title Image
     col1,col2 = st.columns(2,gap= 'medium')
     col1.markdown("## :red[Domain] : Youtube")
-    col1.markdown("## :red[Technologies] : Python,MongoDB compass, Youtube Data API, MySql Workbench, Streamlit")
     col1.markdown("## :red[About the App] : Retrieving the Youtube channels data from the Google API, storing it in a MongoDB as data lake, migrating and transforming data into a SQL database,then querying the data and displaying it in the Streamlit app.")
     col2.markdown("#   ")
     col2.markdown("#   ")
@@ -151,7 +150,7 @@ if selected == "Home":
     
 # EXTRACT AND TRANSFORM PAGE
 if selected == "Extract":
-    tab1,tab2 = st.tabs(["$\huge 📝 EXTRACT $", "$\huge🚀 TRANSFORM $"])
+    tab1,tab2 = st.tabs(["$\huge  EXTRACT $", "$\huge TRANSFORM $"])
     
     # EXTRACT TAB
     with tab1:
